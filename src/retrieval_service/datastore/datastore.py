0b26aac0f06813fc849e3cd9942ff5b344043066
justavejoe <just.ave.joe@gmail.com>
2025-07-13 15:24:11 -0500

    more bugs

--- src/retrieval_service/datastore/datastore.py
+++ src/retrieval_service/datastore/datastore.py
@@ -1,39 +1,19 @@
-# Copyright 2023 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2024 Google LLC
+# (license header)
 
-import csv
 from abc import ABC, abstractmethod
-from datetime import datetime
 from typing import Generic, List, Optional, TypeVar
-
-import models
-
 
 class AbstractConfig(ABC):
     kind: str
 
-
 C = TypeVar("C", bound=AbstractConfig)
-
 
 class classproperty:
     def __init__(self, func):
         self.fget = func
-
     def __get__(self, instance, owner):
         return self.fget(owner)
-
 
 class Client(ABC, Generic[C]):
     @classproperty
@@ -41,9 +21,8 @@ class Client(ABC, Generic[C]):
     def kind(cls):
         pass
 
-
     @abstractmethod
-    async def initialize_data(self) -> None:
+    async def initialize_data(self, paper_chunks: list[dict]) -> None:
         pass
 
     @abstractmethod
@@ -51,8 +30,7 @@ class Client(ABC, Generic[C]):
         self, query_embedding: list[float], top_k: int
     ) -> list[dict]:
         raise NotImplementedError("Subclass should implement this!")
-async def create(config: AbstractConfig) -> Client:
-    for cls in Client.__subclasses__():
-        if config.kind == cls.kind:
-            return await cls.create(config)  # type: ignore
-    raise TypeError(f"No clients of kind '{config.kind}'")
+
+    @abstractmethod
+    async def close(self):
+        pass
