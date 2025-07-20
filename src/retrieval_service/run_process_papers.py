0507b1e837b60824611bf3c20faeefbf5d89dac9
justavejoe <just.ave.joe@gmail.com>
2025-07-20 14:23:42 -0500

    Gemini 2.5 Flash was 1.5

--- src/retrieval_service/run_process_papers.py
+++ src/retrieval_service/run_process_papers.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 PDF_DIRECTORY = "./data/papers_to_process/"
 OUTPUT_CSV_PATH = "./data/processed_papers.csv"
-EMBEDDING_MODEL_NAME = "text-embedding-004"
+EMBEDDING_MODEL_NAME = "gemini-embedding-001"
 
 
 def process_papers():
