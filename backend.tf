6ecf2668748c5a34a82bd714e6b9ca9d1d301252
justavejoe <just.ave.joe@gmail.com>
2025-08-08 19:05:17 -0500

    backend bucket

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "gcs" {
-    bucket  = "sara-1-2-sara-documents-vault" # Use the exact name of your GCS bucket
+    bucket  = "sara-project-v2-sara-documents-vault" # Use the exact name of your GCS bucket
     prefix  = "terraform/state"
   }
 }
