b4191365d8d1c8230c9ace6843e36fd0fcd895ac
justavejoe <just.ave.joe@gmail.com>
2025-08-11 12:55:40 -0500

    totally curious

--- backend.tf
+++ backend.tf
@@ -2,9 +2,8 @@
 
 terraform {
   backend "gcs" {
-    # CORRECTED: This now points to the dedicated Terraform state bucket
-    # that was being used in your Cloud Build environment.
-    bucket  = "sara-v2-tf-state-1b431b9d" 
+    # CORRECTED: This now points to the dedicated Terraform state bucket.
+    bucket  = "sara-v2-tf-state-1b431b9d"
     prefix  = "terraform/state"
   }
 }
