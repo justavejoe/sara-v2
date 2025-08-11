06fce03913beaf809f33579259272f068069b0b3
justavejoe <just.ave.joe@gmail.com>
2025-08-11 09:06:17 -0500

    It's a sescret.

--- secrets.tf
+++ secrets.tf
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,37 +14,67 @@
  * limitations under the License.
  */
 
-## Django Admin Password
-resource "random_password" "cloud_sql_password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
+# Handle Database Instance
+resource "google_sql_database_instance" "main" {
+  count = var.database_type == "postgresql" ? 1 : 0
 
-resource "google_secret_manager_secret" "cloud_sql_password" {
-  project   = module.project-services.project_id
-  secret_id = "genai-cloud-sql-password-${random_id.id.hex}"
-  replication {
-    user_managed {
-      replicas {
-        location = var.region
-      }
+  name             = "sara-db-instance"
+  database_version = "POSTGRES_15"
+  region           = var.region
+  project          = var.project_id
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+  }
+
+  settings {
+    tier              = "db-custom-1-3840"
+    disk_autoresize   = true
+    disk_size         = 10
+    disk_type         = "PD_SSD"
+    user_labels       = var.labels
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.main.id
+    }
+    database_flags {
+      name  = "cloudsql.iam_authentication"
+      value = "on"
     }
   }
 
-  labels = var.labels
-
-  depends_on = [module.project-services]
+  deletion_protection = var.deletion_protection
+  depends_on          = [google_service_networking_connection.private_service_access]
 }
 
-resource "google_secret_manager_secret_iam_binding" "cloud_sql_password" {
-  project   = module.project-services.project_id
-  secret_id = google_secret_manager_secret.cloud_sql_password.id
-  role      = "roles/secretmanager.secretAccessor"
-  members   = ["serviceAccount:${google_service_account.runsa.email}"]
+# Create Database
+resource "google_sql_database" "database" {
+  count = var.database_type == "postgresql" ? 1 : 0
+
+  project         = var.project_id
+  name            = var.db_name
+  instance        = google_sql_database_instance.main[0].name
+  deletion_policy = "ABANDON"
 }
 
-resource "google_secret_manager_secret_version" "cloud_sql_password" {
-  secret      = google_secret_manager_secret.cloud_sql_password.id
-  secret_data = random_password.cloud_sql_password.result
+# Create Cloud SQL User
+resource "google_sql_user" "service" {
+  count = var.database_type == "postgresql" ? 1 : 0
+
+  name     = var.db_user
+  project  = var.project_id
+  instance = google_sql_database_instance.main[0].name
+  # FINAL FIX: This now points to the correctly named password resource from secrets.tf
+  password = random_password.cloud_sql_password.result
+  deletion_policy = "ABANDON"
+}
+
+# Create SQL integration to Vertex AI
+resource "google_project_iam_member" "vertex_integration" {
+  count = var.database_type == "postgresql" ? 1 : 0
+
+  project = var.project_id
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_sql_database_instance.main[0].service_account_email_address}"
 }
